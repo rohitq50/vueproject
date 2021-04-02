e9ba1b89a4d48c7c27f77a6432d5ff2bde1d309f
Rohit Kumar <rohitq50@gmail.com>
2021-04-03 00:49:20 +0530

    build settings

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,29 @@
-// module.exports = {
-// 	css: {
-// 	  loaderOptions: {
-// 		sass: {
-// 			prependData: `
-// 			@import "@/sass/_variables.scss";
-// 			@import "@/sass/_mixins.scss";
-// 		  `
-// 		}
-// 	  }
-// 	}
-// };
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+module.exports = {
+	outputDir: process.env.VUE_APP_BUILD_OUPUT_DIR,
+	assetsDir: process.env.VUE_APP_ASSETS_PATH,
+	// css: {
+	//   loaderOptions: {
+	// 	sass: {
+	// 		prependData: `
+	// 		@import "@/sass/_variables.scss";
+	// 		@import "@/sass/_mixins.scss";
+	// 	  `
+	// 	}
+	//   }
+	// }
+	configureWebpack: {
+		plugins: [
+			  new FileManagerPlugin({
+				events: {
+					onEnd: {
+						copy: [
+							{ source: process.env.VUE_APP_BUILD_OUPUT_DIR, destination: process.env.VUE_APP_PROD_OUPUT_DIR }
+						],
+					}
+				}
+			})
+		]
+	}
+
+};
